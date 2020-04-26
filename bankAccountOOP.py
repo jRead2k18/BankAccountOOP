bc92d626985ee8cd48018ede3a32e2ae085460f5
jRead2k18 <61490233+jRead2k18@users.noreply.github.com>
2020-04-26 04:22:00 -0500

    Add files via upload

---
+++ bankAccountOOP.py
@@ -0,0 +1,28 @@
+class BankAccount:
+    def __init__(self, balance = 0, int_rate = 0.825):
+        self.ir = 1 + int_rate
+        self.bal = balance
+    def deposit(self, amount):
+        self.bal += amount
+        return self
+    def withdraw(self, amount):
+        if (self.bal >= amount):
+            self.bal -= amount
+        else:
+            print("Insufficient funds: Charging a $5 fee")
+            self.bal -= 5
+            print(self.bal)
+        return self
+    def display_account_info(self):
+        print("Balance:", self.bal)
+        return self
+    def yield_interest(self):
+        if (self.bal > 0):
+            self.bal *= self.ir
+        return self
+
+jacob = BankAccount(500)
+avery = BankAccount(250)
+
+jacob.deposit(100).deposit(150).deposit(75).withdraw(50).yield_interest().display_account_info()
+avery.deposit(200).deposit(345).withdraw(65).withdraw(30).withdraw(85).withdraw(50).yield_interest().display_account_info()
